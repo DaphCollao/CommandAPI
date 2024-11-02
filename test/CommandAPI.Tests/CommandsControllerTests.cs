aae9d4139707c92fd5397e5f22cf4d4314d76149
DaphCollao <daphne.collao@gmail.com>
2024-11-02 17:18:17 -0300

    Refactor: reuse of code

--- test/CommandAPI.Tests/CommandsControllerTests.cs
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -4,27 +4,50 @@ using AutoMapper;
 using CommandAPI.Models;
 using CommandAPI.Data;
 using CommandAPI.Profiles;
+using CommandAPI.Dtos;
 using Xunit;
 using CommandAPI.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandAPI.Tests
 {
-    public class CommandsControllerTests
+    public class CommandsControllerTests : IDisposable
     {
+        Mock<ICommandAPIRepo> mockRepo;
+        CommandsProfile realProfile;
+        MapperConfiguration configuration;
+        IMapper mapper;
+
+        public CommandsControllerTests()
+        {
+            mockRepo = new Mock<ICommandAPIRepo>();
+            realProfile = new CommandsProfile();
+            configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
+            mapper = new Mapper(configuration);
+        }
+
+        public void Dispose()
+        {
+            mockRepo = null;
+            mapper = null;
+            configuration = null;
+            realProfile = null;
+        }
+
         [Fact]
         public void GetCommandsItems_ReturnsZeroItems_WhenDBIsEmpty()
         {
-            var mockRepo = new Mock<ICommandAPIRepo>();
-
+            //ARRANGE
             mockRepo.Setup(repo => repo.GetAllCommands())
                                                         .Returns(GetCommands(0));
 
-            var realProfile = new CommandsProfile();
-            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
-            IMapper mapper = new Mapper(configuration);
-
             var controller = new CommandsController(mockRepo.Object, mapper);
+
+            //ACT
+            var result = controller.GetAllCommands();
+
+            //ASSERT
+            Assert.IsType<OkObjectResult>(result.Result);
         }
 
         private List<Command> GetCommands(int num)
