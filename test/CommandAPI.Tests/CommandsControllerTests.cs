94cb2afea00729edf3e4da762ab482d6502a5c6a
DaphCollao <daphne.collao@gmail.com>
2024-11-02 20:06:13 -0300

    add more test

--- test/CommandAPI.Tests/CommandsControllerTests.cs
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -13,10 +13,10 @@ namespace CommandAPI.Tests
 {
     public class CommandsControllerTests : IDisposable
     {
-        Mock<ICommandAPIRepo> mockRepo;
-        CommandsProfile realProfile;
-        MapperConfiguration configuration;
-        IMapper mapper;
+        Mock<ICommandAPIRepo>? mockRepo;
+        CommandsProfile? realProfile;
+        MapperConfiguration? configuration;
+        IMapper? mapper;
 
         public CommandsControllerTests()
         {
@@ -50,6 +50,53 @@ namespace CommandAPI.Tests
             Assert.IsType<OkObjectResult>(result.Result);
         }
 
+        [Fact]
+        public void GetAllCommands_ReturnsOneItem_WhenDBHasOneResource()
+        {
+            //ARRANGE
+            mockRepo.Setup(repo => repo.GetAllCommands()).Returns(GetCommands(1));
+
+            var controller = new CommandsController(mockRepo.Object, mapper);
+            Console.WriteLine($"controller: {controller}");
+
+            //ACT
+            var result = controller.GetAllCommands();
+            Console.WriteLine($"Result: {result.Result}");
+            //ASSERT
+            var okResult = result.Result as OkObjectResult;
+            var commands = okResult.Value as List<CommandReadDto>;
+            Assert.Single(commands);
+        }
+
+        [Fact]
+        public void GetAllCommands_Returns200Ok_WhenDBHasOneResource()
+        {
+            //ARRANGE
+            mockRepo.Setup(repo => repo.GetAllCommands()).Returns(GetCommands(1));
+
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            //ACT
+            var result = controller.GetAllCommands();
+
+            //ASSERT
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void GetAllCommands_ReturnsCorrecType_WhenDBHasOneResource()
+        {
+            //ARRANGE
+            mockRepo.Setup(repo => repo.GetAllCommands()).Returns(GetCommands(1));
+
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            //ACT
+            var result = controller.GetAllCommands();
+
+            //ASSERT
+            Assert.IsType<ActionResult<IEnumerable<CommandReadDto>>>(result);
+        }
         private List<Command> GetCommands(int num)
         {
             var commands = new List<Command>();
